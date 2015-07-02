a609f9466df481fc6b95aaae7a1386952961c5ea
iBelieve <momen.1993@live.com>
2015-07-02 08:39:30 +0200

    C Programming Course Advanced level

---
+++ Programming Course/C89/1-Arrays & Pointers/Precedence in pointer operations/Precedence in pointer operations/PointerOrder.c
@@ -0,0 +1,24 @@
+// Source: C Primer Plus 6th Edition
+// order.c -- precedence in pointer operations 
+
+#include <stdio.h>
+
+int data[2]= {100, 200};
+int moredata[2]= {300, 400};
+
+int main(void)
+{
+    int *p1, *p2, *p3;
+
+    p1= p2 = data;
+    p3= moredata;
+    printf("  *p1 = %d,   *p2 = %d,     *p3 = %d\n",
+              					*p1, *p2, *p3);
+    printf("*p1++ = %d, *++p2 = %d, (*p3)++ = %d\n", 
+    					*p1++, *++p2, (*p3)++);
+    printf("  *p1 = %d,   *p2 = %d,     *p3 = %d\n",
+              					*p1, *p2, *p3);
+
+	system("pause");									
+    return 0;
+}
---
+++ Programming Course/C89/3- Storage, Linkage & memory management/Local static variables/Local static variables/localstatic.c
@@ -0,0 +1,26 @@
+// Source: C Primer Plus 6th Edition
+/* loc_stat.c -- using a local static variable */
+#include <stdio.h>
+void trystat(void);
+ 
+int main(void)
+{
+    int count;
+ 
+    for (count = 1; count <= 3; count++)
+    {
+        printf("Here comes iteration %d:\n", count);
+        trystat();
+    }
+ 
+	  system("pause");
+      return 0;
+}
+ 
+void trystat(void)
+{
+    int fade = 1;
+    static int stay = 1;
+ 
+    printf("fade = %d and stay = %d\n", fade++, stay++);
+}
